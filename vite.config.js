2a01dede2a41a28c963c36e59cb1ef5c1e9fa372
jettpbaker <jett.p.baker@gmail.com>
2024-10-18 15:12:43 +1000

    Added initial dependencies/packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-styled-components"],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+  },
+});
